07f13dce1cd3fdc5dceaa581dd47581e765f4391
askashahira <askasahira505@gmail.com>
2026-05-04 20:44:33 +0700

    akun admin auto ada

---
+++ accounts/management/commands/createadmin.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+import os
+
+class Command(BaseCommand):
+    help = 'Create default admin user'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+        email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@gmail.com')
+        password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin123')
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password
+            )
+            self.stdout.write(self.style.SUCCESS('Admin created'))
+        else:
+            self.stdout.write('Admin already exists')
